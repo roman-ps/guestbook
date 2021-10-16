e2a22ff2deaeb73cc8b6abd4c4af01d8a0bae078
Pozharov Roman <ru.roman.ps@gmail.com>
2021-10-16 20:16:37 +0400

    Достал значения полей формы по имени

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,18 +3,15 @@ import classes from './Form.module.css';
 import {renderData, getLastId} from './../../index';
 
 const Form = (props) => {
-  const name = React.createRef();
-  const mail = React.createRef();
-  const city = React.createRef();
-  const text = React.createRef();
+  const addReview = (evt) => {
+    evt.preventDefault();
 
-  const addReview = () => {
     const newReview = {
       id: getLastId(),
-      name: name.current.value,
-      mail: mail.current.value,
-      city: city.current.value,
-      text: text.current.value,
+      name: evt.target.name.value,
+      mail: evt.target.mail.value,
+      city: evt.target.city.value,
+      text: evt.target.text.value,
     }
 
     props.data.push(newReview);
@@ -22,24 +19,24 @@ const Form = (props) => {
   };
 
   return (
-    <form className={classes.container}>
+    <form className={classes.container} action='' method='POST' onSubmit={addReview}>
       <div className={classes.item}>
         <label htmlFor='name'>Имя: </label>
-        <input className={classes.name} placeholder='Ваше имя' type='text' id='name' minLength='3' required ref={name}/>
+        <input className={classes.name} placeholder='Ваше имя' type='text' id='name' minLength='3' required />
       </div>
       <div className={classes.item}>
         <label htmlFor='mail'>Почта: </label>
-        <input placeholder='Ваша почта' type='email' id='mail' required ref={mail}/>
+        <input placeholder='Ваша почта' type='email' id='mail' required />
       </div>
       <div className={classes.item}>
         <label htmlFor='city'>Город: </label>
-        <input placeholder='Откуда вы' type='text' id='city' required ref={city}/>
+        <input placeholder='Откуда вы' type='text' id='city' required />
       </div>
       <div className={classes.item}>
         <label htmlFor='text'>Текст: </label>
-        <textarea placeholder='Напишите свой отзыв' id='text' minLength='5' required ref={text}/>
+        <textarea placeholder='Напишите свой отзыв' id='text' minLength='5' required />
       </div>
-      <button onClick={addReview} className={classes.btn} type='button'>Добавить отзыв</button>
+      <button className={classes.btn} type='submit'>Добавить отзыв</button>
     </form>
   );
 };
