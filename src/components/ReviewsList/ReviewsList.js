b2b1cbb97c7b4128644c8a1925f0fc1609f02ca4
Pozharov Roman <ru.roman.ps@gmail.com>
2021-10-16 20:18:52 +0400

    Убрал ключ по индексу в ReviewsList

--- src/components/ReviewsList/ReviewsList.js
+++ src/components/ReviewsList/ReviewsList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReviewItem from './ReviewItem/ReviewItem';
+import classes from './ReviewsList.module.css';
 
 const ReviewsList = (props) => {
   console.log(props.data)
   const additems = props.data;
-  const elements = additems.map((elem, index) => {
-    return <ReviewItem name={elem.name} mail={elem.mail} city={elem.city} text={elem.text} key={index}/>
+  const elements = additems.map((elem) => {
+    return <ReviewItem name={elem.name} mail={elem.mail} city={elem.city} text={elem.text} key={id}/>
   });
 
   return (
-    <div>
+    <div className={classes.list}>
       {elements}
     </div>
   );
