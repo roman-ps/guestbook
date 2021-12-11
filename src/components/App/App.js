bfdca379b059740b8f8e61786b70e2772ab73494
Pozharov Roman <ru.roman.ps@gmail.com>
2021-12-11 09:47:39 +0400

    Добавил вывод сообщения о ненайденных совпадениях

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,40 +5,7 @@ import Form from './Form/Form';
 import Subtitle from './Subtitle/Subtitle';
 import Reviews from './Reviews/Reviews';
 
-const reviewsAll = [
-  {
-    name: 'Роман',
-    mail: 'roman@gmail.com',
-    city: 'Samara',
-    text: 'tiogjtogjotij jthoijt r ojtohi jrtoihjrtio jroti jrtij ohi',
-    sex: 'Мужской',
-    id: 1,
-  },
-  {
-    name: 'Катенька',
-    mail: 'katya@gmail.com',
-    city: 'Samara',
-    text: '5675756 56756 r 567567 8797897 jroti 5464 ohi',
-    sex: 'Женский',
-    id: 2,
-  },
-  {
-    name: 'Сергей',
-    mail: 'sergey@gmail.com',
-    city: 'SergTown',
-    text: '989808 jthoijt r 657575 67869789 jroti jrtij ohi',
-    sex: 'Мужской',
-    id: 3,
-  },
-  {
-    name: 'Касиопея',
-    mail: 'kas@gmail.com',
-    city: 'Pompei',
-    text: 'tiogjtogjotij jthoijt r hjkhjkhj uiouiou jroti uiouiou ohi',
-    sex: 'Женский',
-    id: 4,
-  },
-];
+const reviewsAll = [];
 
 const App = () => {
   const [reviews, setReviews] = useState(reviewsAll);
--- src/components/App/Reviews/ReviewsList/ReviewsList.js
+++ src/components/App/Reviews/ReviewsList/ReviewsList.js
@@ -4,9 +4,13 @@ import classes from './ReviewsList.module.css';
 
 const ReviewsList = (props) => {
   const dataItems = props.items;
-  const elements = dataItems.map((elem) => (
+  let elements = <p>Ничего не найдено</p>;
+
+  if (dataItems.length > 0) {
+    elements = dataItems.map((elem) => (
     <ReviewItem name={elem.name} mail={elem.mail} city={elem.city} text={elem.text} key={elem.id} id={elem.id} sex={elem.sex}/>
-  ));
+    ))
+  };
 
   return (
     <div className={classes.list}>
