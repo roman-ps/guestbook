85505a0217c3fe1ffbc31e7103a95e01db1e512f
Pozharov Roman <ru.roman.ps@gmail.com>
2022-01-14 22:21:35 +0400

    Добавил тестовые данные и ф-ию загрузки сторонних данных

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,17 +6,69 @@ import Reviews from './Reviews/Reviews';
 
 const reviewsAll = [
   {
-    name: 'Landini',
-    mail: 'landini@land.com',
-    city: 'Florida',
-    text: 'Landini love Florida',
-    sex: 'Мужской',
-    id: 1,
+    "name": "Leanne Graham",
+    "mail": "Sincere@april.biz",
+    "city": "Gwenborough",
+    "text": "Leanne Graham from Gwenborough",
+    "sex": "Мужской",
+    "id": 1
   },
+  {
+    "name": "Ervin Howell",
+    "mail": "Shanna@melissa.tv",
+    "city": "Wisokyburgh",
+    "text": "Ervin Howell from Wisokyburgh",
+    "sex": "Женский",
+    "id": 2
+  },
+  {
+    "name": "Clementine Bauch",
+    "mail": "Nathan@yesenia.net",
+    "city": "McKenziehaven",
+    "text": "Clementine Bauch from McKenziehaven",
+    "sex": "Женский",
+    "id": 3
+  },
+  {
+    "name": "Patricia Lebsack",
+    "mail": "Julianne.OConner@kory.org",
+    "city": "South Elvis",
+    "text": "Patricia Lebsack from South Elvis",
+    "sex": "Женский",
+    "id": 4
+  },
+  {
+    "name": "Mrs. Dennis Schulist",
+    "mail": "Karl_Dach@jasper.info",
+    "city": "South Christy",
+    "text": "Mrs. Dennis Schulist from South Christy",
+    "sex": "Мужской",
+    "id": 5
+  },
+  {
+    "name": "Kurtis Weissnat",
+    "mail": "Telly.Hoeger@billy.biz",
+    "city": "Howemouth",
+    "text": "Kurtis Weissnat from Howemouth",
+    "sex": "Мужской",
+    "id": 6
+  }
 ];
+
+const DATA_URL = './reviews.json';
 
 const App = () => {
   const [reviews, setReviews] = useState(reviewsAll);
+
+  const loadData = () => {
+    return fetch(DATA_URL)
+      .then((response) => {
+        return response.json
+      })
+      .then((data) => {
+        setReviews(data);
+      })
+  };
 
   const getNextId = () => {
     return reviews.reduce(function(a,b)  {
@@ -27,6 +79,7 @@ const App = () => {
   const onFinalSaveData = (enterData) => {
     const actualData = {...enterData, id: getNextId()};
     setReviews([actualData, ...reviews]);
+    loadData()
   };
 
   const deleteItemById = (id) => {
