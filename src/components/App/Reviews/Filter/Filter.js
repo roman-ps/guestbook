ff181055eea26a6a9528397b1b48abfa0b99d897
Pozharov Roman <ru.roman.ps@gmail.com>
2022-01-21 20:41:33 +0400

    Добавил фильтр возраста

--- src/components/App/Reviews/Filter/Filter.js
+++ src/components/App/Reviews/Filter/Filter.js
@@ -2,18 +2,35 @@ import React from 'react';
 import classes from './Filter.module.css';
 
 const Filter = (props) => {
-  const selectFilterHandler = (evt) => {
-    props.selectFilterHandler(evt.target.value);
+  const toggleGenderFilterHandler = (evt) => {
+    props.selectGenderFilterHandler(evt.target.value);
+  };
+
+  const toggleAgeFilterHandler = (evt) => {
+    props.selectAgeFilterHandler(evt.target.value);
   };
 
   return (
     <div className={classes.filter}>
-      <p className={classes.title}>Фильтр</p>
-      <select name='select' onChange={selectFilterHandler}>
-        <option value=''></option>
-        <option value='male'>Мужской</option>
-        <option value='female'>Женский</option>
-      </select>
+      <div className={classes['filter-container']}>
+        <p className={classes.title}>Пол</p>
+        <select name='select' onChange={toggleGenderFilterHandler}>
+          <option value=''></option>
+          <option value='male'>Мужской</option>
+          <option value='female'>Женский</option>
+        </select>
+      </div>
+      <div className={classes['filter-container']}>
+        <p className={classes.title}>Возраст</p>
+        <select name='select' onChange={toggleAgeFilterHandler}>
+          <option value=''></option>
+          <option value='young'>до 20</option>
+          <option value='adult'>21-50</option>
+          <option value='old'>51-65</option>
+          <option value='superstar'>от 66</option>
+        </select>
+      </div>
+
     </div>
   )
 };
--- src/components/App/Reviews/Reviews.js
+++ src/components/App/Reviews/Reviews.js
@@ -4,24 +4,37 @@ import Filter from './Filter/Filter';
 import classes from './Reviews.module.css';
 
 const Reviews = (props) => {
-  const [filteredSex, setFilteredSex] = useState('');
-
-  const onSelectFilterHandler = (selectedSex) => {
-    setFilteredSex(selectedSex);
+  const [filteredGender, setFilteredGender] = useState('');
+  const [filteredAge, setFilteredAge] = useState([0, 150]);
+  const AgeTypes = {
+    young: [0, 20],
+    adult: [21, 50],
+    old: [51, 65],
+    superstar: [66, 150],
   };
 
+  const onSelectFilterHandler = (filteredGender) => {
+    setFilteredGender(filteredGender);
+    console.log(filteredGender);
+  };
+
+  const onSelectAgeHandler = (age) => {
+    setFilteredAge(AgeTypes[age]);
+    console.log(filteredAge);
+  }
+
   const filteredReviews = props.items.filter((elem) => {
-    return (elem.gender === filteredSex || filteredSex === '');
+    return ((elem.gender === filteredGender || filteredGender === '') && (elem.age < filteredAge[1] && elem.age > filteredAge[0]));
   });
 
   const saveIdInReviews = (id) => {
     props.passIdInApp(id);
-  }
+  };
 
   return (
     <main className={classes.main}>
       <h2 className={classes.title}>Reviews</h2>
-      <Filter selectFilterHandler={onSelectFilterHandler} />
+      <Filter selectGenderFilterHandler={onSelectFilterHandler} selectAgeFilterHandler={onSelectAgeHandler}/>
       <ReviewsList passIdInReviews={saveIdInReviews} items={filteredReviews} />
     </main>
   )
